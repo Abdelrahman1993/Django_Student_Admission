fe61db44dad45f175dbc20fed7de795e2a63e0fb
Abdelrahman Ahmed <AbdelrahmanAbdelfattah@yahoo.com>
2019-05-18 10:50:35 +0200

    images of readme

--- admission/settings.py
+++ admission/settings.py
@@ -74,22 +74,13 @@ WSGI_APPLICATION = 'admission.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'evs',
-        'USER': 'abdo',
-        'PASSWORD': 'abdo9111993',
-        'HOST': 'localhost',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
+
 
 
 # Password validation
